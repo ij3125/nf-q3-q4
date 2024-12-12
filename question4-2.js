1d58e6f44ab1c49b697562a34e4c2a37e6685254
ij3125 <igutituba@gmail.com>
2024-12-12 16:24:30 +0900

    question4-2(update).js

--- question4-2.js
+++ question4-2.js
@@ -1,21 +1,22 @@
 // p21
-const p21 = new Promise((resolve, reject) => {
-  resolve("Success!");
-});
+const p21 = Promise.resolve("Resolved value");
 
 // f21
 const f21 = (value) => {
-  console.log("f21 called with value:", value);
-  throw new Error("Error in f21");
+  console.log("f21 received:", value);
+  // 非同期エラー
+  return new Promise((_, reject) => {
+    setTimeout(() => reject(new Error("Error in f21")), 0);
+  });
 };
 
 // f22
 const f22 = (error) => {
-  console.log("f22 called with error:", error.message);
+  console.log("f22 handled:", error.message);
 };
 
-console.log("Program 21:");
-p21.then(f21).catch(f22);
+// プログラム21
+p21.then(f21).catch(f22)
 
-console.log("Program 22:");
-p21.then((value) => { f21(value); }).catch(f22);
+// プログラム22
+p21.then((value) => {f21(value);}).catch(f22)
